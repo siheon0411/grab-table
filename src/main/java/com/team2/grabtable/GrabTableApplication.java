a99218bbad9f9eac80b3224e583198c83ee452ec
Siheon Kim <siheon0411@gmail.com>
2025-05-08 14:45:51 +0900

    first commit

---
+++ src/main/java/com/team2/grabtable/GrabTableApplication.java
@@ -0,0 +1,13 @@
+package com.team2.grabtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrabTableApplication.class, args);
+    }
+
+}
