4c4842aff779b5acc47939c4e069b135f0739994
guntaek <rjsxor7584@naver.com>
2025-05-09 14:00:23 +0900

    fix : 가게 CRUD 기능 수정

--- src/main/java/com/team2/grabtable/domain/store/service/StoreService.java
+++ src/main/java/com/team2/grabtable/domain/store/service/StoreService.java
@@ -3,6 +3,7 @@ package com.team2.grabtable.domain.store.service;
 import com.team2.grabtable.config.OwnerDetails;
 import com.team2.grabtable.domain.store.dto.StoreDto;
 import com.team2.grabtable.domain.store.dto.StoreImageDto;
+import com.team2.grabtable.domain.store.dto.StoreRegisterDto;
 import com.team2.grabtable.domain.store.dto.StoreResultDto;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,9 +17,9 @@ public interface StoreService {
 
     StoreImageDto getStoreImage(Long storeId);
 
-    StoreResultDto insertStore(OwnerDetails ownerDetails, StoreDto storeDto, MultipartFile imageFile) throws IOException;
+    StoreResultDto insertStore(OwnerDetails ownerDetails, StoreRegisterDto storeRegisterDto) throws IOException;
 
-    StoreResultDto updateStore(OwnerDetails ownerDetails, StoreDto storeDto);
+    StoreResultDto updateStore(OwnerDetails ownerDetails, Long storeId, StoreRegisterDto storeRegisterDto) throws IOException;
 
     StoreResultDto deleteStore(Long storeId);
 
